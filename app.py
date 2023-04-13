0d802ad2e6493dfcea3199ef5784c312822d8aaf
faizer1989 <faizer@inbox.ru>
2023-04-13 22:14:52 +0800

    changes

--- app.py
+++ app.py
@@ -13,18 +13,39 @@ with open('catboost_heart.pkl', 'rb') as f:
 # Load the dataset
 df = pd.read_csv('heart.csv')
 
-# Add a title and a brief description of the app
-st.title("OBISCOPE")
-st.write("The heart disease prediction app has been developed by 'Precious Heart' team from Nigeria 🇳🇬 for Technovation World Challenge 2023.")
-
 # Load an image file from your local machine or a URL
-image = Image.open('school_logo.jpg')  # Replace with the correct path to your image file or a URL
+school_image = Image.open('school_logo.jpg')  # Replace with the correct path to your image file or a URL
+app_image = Image.open('app_logo.jpg')  # Replace with the correct path to your image file or a URL
+# Add a title and a brief description of the app
 
-# Display the image in the app after the description
-st.image(image, caption='Your image caption', use_column_width=True)
+
+# Create responsive layout columns
+left_column, right_column = st.columns(2)
+
+# Insert the image in the middle column
+with right_column:
+    st.image(app_image, width=150)
+
+with left_column:
+    st.title("OBISCOPE")
+
+st.write("___")
+left_column1, right_column1 = st.columns(2)
+
+# Insert the image in the middle column
+with right_column1:
+    st.image(school_image, width=70)
+
+with left_column1:
+    st.markdown("<i>The heart disease prediction app has been developed by 'Precious Heart' team from Our Lady's Sec. Sch. Nnobi, Nigeria for Technovation World Challenge 2023.</i>", unsafe_allow_html=True)
+    
+st.write("___")   
+    
+  
+
 
 # Display a sample of the dataset
-st.write("Here's a sample of the dataset:")
+st.write("Dataset example that were used to build AI prediction model:")
 st.write(df.head())    
 
 # Define a function to preprocess the user input
@@ -40,7 +61,7 @@ def preprocess_input(age, sex, cp_type, bp, chol, fasting_bs, ecg, max_hr, ex_an
 # Define the Streamlit app
 def app():
     # Define the input fields
-    st.title('Heart Disease Prediction')
+    st.title("Enter the vital signs to predict CAD")
     age = st.slider('Age', min_value=0, max_value=120, step=1)
     sex = st.radio('Sex', ['Male', 'Female'])
     cp_type = st.selectbox('Chest Pain Type', ['Typical Angina', 'Atypical Angina', 'Non-Anginal Pain', 'Asymptomatic'])
